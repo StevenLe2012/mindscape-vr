717a6fac0a2f36138bb67a3139f0489399eea941
Steven Le <64396816+StevenLe2012@users.noreply.github.com>
2023-03-06 20:18:08 -0800

    Added ChatGPT Unofficial Unity Package
    https://github.com/srcnalt/OpenAI-Unity

---
+++ Assets/Samples/OpenAI Unity/0.1.6/ChatGPT/ChatGPT.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace OpenAI
+{
+    public class ChatGPT : MonoBehaviour
+    {
+        [SerializeField] private InputField inputField;
+        [SerializeField] private Button button;
+        [SerializeField] private Text textArea;
+
+        private OpenAIApi openai = new OpenAIApi();
+
+        private string userInput;
+        private string Instruction = "Act as a random stranger in a chat room and reply to the questions.\nQ: ";
+
+        private void Start()
+        {
+            button.onClick.AddListener(SendReply);
+        }
+
+        private async void SendReply()
+        {
+            userInput = inputField.text;
+            Instruction += $"{userInput}\nA: ";
+            
+            textArea.text = "...";
+            inputField.text = "";
+
+            button.enabled = false;
+            inputField.enabled = false;
+            
+            // Complete the instruction
+            var completionResponse = await openai.CreateCompletion(new CreateCompletionRequest()
+            {
+                Prompt = Instruction,
+                Model = "text-davinci-003",
+                MaxTokens = 128
+            });
+
+            if (completionResponse.Choices != null && completionResponse.Choices.Count > 0)
+            {
+                textArea.text = completionResponse.Choices[0].Text;
+                Instruction += $"{completionResponse.Choices[0].Text}\nQ: ";
+            }
+            else
+            {
+                Debug.LogWarning("No text was generated from this prompt.");
+            }
+
+            button.enabled = true;
+            inputField.enabled = true;
+        }
+    }
+}
