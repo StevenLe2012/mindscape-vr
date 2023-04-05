39dbbabb4b13109153083357e965feb8f69de3eb
Noegenesis <104540327+Noegnesis@users.noreply.github.com>
2023-04-04 21:15:30 -0700

    UI Updates

---
+++ Assets/_Scripts/WristMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WristMenu : MonoBehaviour
+{
+ 
+    [SerializeField] private TMP_Text textArea;
+
+}
