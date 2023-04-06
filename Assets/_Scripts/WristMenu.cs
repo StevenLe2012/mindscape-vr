bc40d316ef5aeb6f0a180f8628e9786e225d8815
Noegenesis <104540327+Noegnesis@users.noreply.github.com>
2023-04-05 18:07:26 -0700

    CompleteXRRig_WristAdjustment

--- Assets/_Scripts/WristMenu.cs
+++ Assets/_Scripts/WristMenu.cs
@@ -9,4 +9,18 @@ public class WristMenu : MonoBehaviour
  
     [SerializeField] private TMP_Text textArea;
 
+    public void SendAdd()
+    {
+        textArea.text = "Pressed Add";
+    }
+    
+    public void SendReset()
+    {
+        textArea.text = "Pressed Reset";
+    }
+    
+    public void SendDiffusion()
+    {
+        textArea.text = "Pressed Stable Diffusion";
+    }
 }
